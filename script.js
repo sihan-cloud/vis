6c10414af746d62fa677a6105e2e284f2222bdd7
wsh <1487358848@qq.com>
2023-04-19 17:45:51 +0800

    delete useless file

--- script.js
+++
@@ -1,9 +0,0 @@
-const panels=document.querySelectorAll('.panel');
-panels.forEach((item)=>{
-    item.addEventListener('click',()=>{
-        panels.forEach((item)=>{
-            item.classList.remove('active');
-        })
-        item.classList.add('active');
-    });
-});
